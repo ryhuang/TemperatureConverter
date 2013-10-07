b514c2df691f81bb9c43dcc4713ec04855332413
Richard Huang <ryhuang@Richards-MacBook-Air.local>
2013-10-06 23:09:50 -0700

    Initial Commit

---
+++ TemperatureConversion/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConversion
+//
+//  Created by Richard Huang on 10/6/13.
+//  Copyright (c) 2013 Rivi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConversion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TemperatureConversion
+//
+//  Created by Richard Huang on 10/6/13.
+//  Copyright (c) 2013 Rivi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
