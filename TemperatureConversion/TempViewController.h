1dc5f38471e20ad51dd31d52b65262a3c3b1fc6a
Richard Huang <sdsoxfan@gmail.com>
2013-10-09 18:13:22 -0700

    first commit

---
+++ TemperatureConversion/TempViewController.h
@@ -0,0 +1,13 @@
+//
+//  TempViewController.h
+//  TemperatureConversion
+//
+//  Created by Richard Huang on 10/6/13.
+//  Copyright (c) 2013 Rivi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempViewController : UIViewController
+
+@end
---
+++ TemperatureConversion/TempViewController.m
@@ -0,0 +1,38 @@
+//
+//  TempViewController.m
+//  TemperatureConversion
+//
+//  Created by Richard Huang on 10/6/13.
+//  Copyright (c) 2013 Rivi. All rights reserved.
+//
+
+#import "TempViewController.h"
+
+@interface TempViewController ()
+
+@end
+
+@implementation TempViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
